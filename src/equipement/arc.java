8c489d01bc2e18861e1ccf9b2218c00f5dc3f960
Azarom <60900208+Azarom@users.noreply.github.com>
2022-04-15 16:51:05 +0200

    Update arc.java

--- src/equipement/arc.java
+++ src/equipement/arc.java
@@ -1,1 +1,41 @@
+package equipement;
+
+import etreVivant.EtreVivant;
+
+public class Arc extends Arme {
+	private int nombreFleche;
+	private String name;
+	public Arc(int f) {
+		super(30, "arc");
+		nombreFleche = f;
+		name = "arc";
+		// TODO Auto-generated constructor stub
+	}
+	public int nombreFleche() {
+		return this.nombreFleche;
+	}
+	public void attaquer(EtreVivant d) {
+		if(this.nombreFleche > 0 ) {
+		d.setVie(d.getVie()-this.degat);
+		this.nombreFleche--;
+		if (d.getVie() < 0){
+			d.setVie(0);
+		}
+		String a = d.getNom() + "subit une attaque violente";
+		if (d.getVie() == 0) {
+			a = a + " trop violente pour survivre"; 
+			d.mourir();
+		}
+		else {
+			a = a + " mais il  parvient a se relever"; 
+		}
+		 
+	}
+		
+	
+}
+	public String getNom() {
+		return this.name;
+	}
+}
 
