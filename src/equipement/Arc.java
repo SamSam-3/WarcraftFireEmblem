a5a5bddece5a864cb87f67b442f7be9efb30c375
Azarom <60900208+Azarom@users.noreply.github.com>
2022-05-16 19:05:18 +0200

    Commentaires

--- src/equipement/Arc.java
+++ src/equipement/Arc.java
@@ -5,9 +5,10 @@ import etreVivant.EtreVivant;
 public class Arc extends Arme {
 	private int nombreFleche;
 	private String name;
-	public Arc(String n,int d) {
+	public Arc(String n,int d) { //constructeur
 		degat = d;
 		name = n;
+		nomnreFleche = 30;
 		
 	}
 	public String getNom() {
@@ -19,15 +20,15 @@ public class Arc extends Arme {
 	}
 	public void attaquer(EtreVivant d) {
 		if(this.nombreFleche > 0 ) {
-		d.PrendreCoup(this.degat);
-		this.nombreFleche--;
+		d.PrendreCoup(this.degat);  //application des dégats
+		this.nombreFleche--; //on retire une flèche
 		if (d.getVie() < 0){
-			d.setVie(0);
+			d.setVie(0); 
 		}
 		String a = d.getNom() + "subit une attaque violente";
-		if (d.getVie() == 0) {
+		if (d.getVie() == 0) { //si les pv de l'adversaire sont null
 			a = a + " trop violente pour survivre"; 
-			d.mourir();
+			d.mourir(); //mort de l'adversaire
 		}
 		else {
 			a = a + " mais il  parvient a se relever"; 
