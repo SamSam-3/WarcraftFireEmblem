b0ebde0b46e1ef67ef6f3799a85f506143801d5e
Azarom <60900208+Azarom@users.noreply.github.com>
2022-05-16 19:20:11 +0200

    Commentaires

--- src/equipement/Armure.java
+++ src/equipement/Armure.java
@@ -3,21 +3,21 @@ import etreVivant.EtreVivant;
 
 public class Armure {
 
-	private int PA;
+	private int PA; //point d'armure
 	private String nom;
 	protected EtreVivant proprietaire;
 	
-	public Armure(String n,int pa){
+	public Armure(String n,int pa){ //constructeur
 		nom = n;
 		PA = pa;
 	}
 	
-	public int prendreCoup(int dg){
-		int a = 0;
-		this.setPA(this.getPA()-dg);
-		if (this.getPA() >= 0) {
-			a = Math.abs(this.getPA());
-			this.getProprietaire().perdreArmure();
+	public int prendreCoup(int dg){ //calcul de prise de dégat , renvois le reste des dégats non réduits
+		int a = 0; //variable contenant le reste de dégat
+		this.setPA(this.getPA()-dg); //valeur d'armure - dgt
+		if (this.getPA() >= 0) { //si l'armure n'a plus de point d'amure
+			a = Math.abs(this.getPA()); //le reste des dégats prend l'opposé de la soustraction
+			this.getProprietaire().perdreArmure(); //le personnage perd son armure
 		}
 		return a; 
 	}
@@ -25,7 +25,7 @@ public class Armure {
 	public void lacher() {
 		this.proprietaire = null;
 	}
-	
+	//getter et setters
 
 	public int getPA() {
 		return PA;
