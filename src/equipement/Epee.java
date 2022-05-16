4c6631f4b940ef503069cfe28b7593b8afcbbb4d
Azarom <60900208+Azarom@users.noreply.github.com>
2022-05-16 19:20:35 +0200

    Update Epee.java

--- src/equipement/Epee.java
+++ src/equipement/Epee.java
@@ -2,11 +2,12 @@ package equipement;
 
 public class Epee extends Arme{
 	private String name;
-	public Epee( String n,int d) {
+	public Epee( String n,int d) { //constructeur
 		degat = d;
 		name = n;
 		
 	}
+	//getter
 	public String getNom() {
 		return this.name;
 	}
