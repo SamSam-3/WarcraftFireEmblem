f1bc99479f268301dd46f77738d5781d1ed7a625
Azarom <60900208+Azarom@users.noreply.github.com>
2022-05-24 19:46:16 +0200

    Update Epee.java

--- src/equipement/Epee.java
+++ src/equipement/Epee.java
@@ -11,4 +11,23 @@ public class Epee extends Arme{
 	public String getNom() {
 		return this.name;
 	}
+	public void attaquer(EtreVivant d) {
+
+		d.PrendreCoup(this.degat);
+
+		if (d.getVie() < 0){
+			d.setVie(0);
+		}
+		String a = d.getNom() + "subit une attaque violente";
+		if (d.getVie() == 0) {
+			a = a + " trop violente pour survivre"; 
+		}
+		else {
+			a = a + " mais il  parvient a se relever"; 
+		}
+		 
+	}
+		
+	
+}
 }
